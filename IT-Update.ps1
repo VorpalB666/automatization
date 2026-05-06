4ed2681552391f0d6983b0ca1383954226410cb7
VorpalB666 <vorpal_blade@tuta.io>
2026-05-06 15:32:46 +0200

    IT-Update erstellt und eingerichtet

---
+++ IT-Update.ps1
@@ -0,0 +1,44 @@
+Write-Host "IT-Update wird gestartet..." -ForegroundColor Red
+
+# === GitHub Trending ===
+$EcosiaPath = "C:\Users\Stefa\AppData\Local\EcosiaBrowser\Application\ecosiabrowser.exe"
+
+if (Test-Path $EcosiaPath) {
+    Start-Process $EcosiaPath "https://github.com/trending"
+    Start-Process $EcosiaPath "https://github.com/explore"
+}
+else {
+    Write-Warning "GitHub Trending nicht gefunden – Pfad prüfen!"
+}
+
+Start-Sleep -Seconds 2
+
+# === VSCode ===
+# Typischer Pfad (ggf. anpassen)
+$VSCodePath = "C:\Users\Stefa\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+
+if (Test-Path $VSCodePath) {
+    Start-Process $VSCodePath
+}
+else {
+   Write-Warning "Proton Mail Client nicht gefunden – Pfad prüfen!"
+}
+
+Start-Process "OneNote"
+
+Start-Sleep -Seconds 2
+
+# === Further Internet Sources ===
+$EcosiaPath = "C:\Users\Stefa\AppData\Local\EcosiaBrowser\Application\ecosiabrowser.exe"
+
+if (Test-Path $EcosiaPath) {
+    Start-Process $EcosiaPath "https://www.heise.de/"
+    Start-Process $EcosiaPath "https://www.security-insider.de/"
+    Start-Process $EcosiaPath "https://techcrunch.com/latest/"
+}
+else {
+    Write-Warning "Further Internet Sources nicht gefunden – Pfad prüfen!"
+}
+
+
+Write-Host "IT-Update ready" -ForegroundColor Green
