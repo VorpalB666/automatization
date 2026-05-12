536b64dfa8d1bea360e3a1c1ee9327478f61b422
VorpalB666 <vorpal_blade@tuta.io>
2026-05-12 18:03:29 +0200

    Backup überarbeitet und angepasst

--- backup_auto.ps1
+++ backup_auto.ps1
@@ -1,11 +1,10 @@
 $Backups = @{
-    "C:\Users\Stefa\OneDrive\Dokumente" = "E:\Backup\Dokumente"
-    "C:\Users\Stefa\OneDrive\Gesundheit" = "E:\Backup\Gesundheit"
-    "C:\Users\Stefa\OneDrive\IT" = "E:\Backup\IT"
-    "C:\Users\Stefa\OneDrive\Wohnung" = "E:\Backup\Wohnung"
+    "C:\Users\Stefa\OneDrive\Dokumente"  = "E:\Backup\Dokumente"
+    "C:\Users\Stefa\OneDrive\Gesundheit"= "E:\Backup\Gesundheit"
+    "C:\Users\Stefa\OneDrive\IT"         = "E:\Backup\IT"
+    "C:\Users\Stefa\OneDrive\Wohnung"    = "E:\Backup\Wohnung"
 }
 
 foreach ($pair in $Backups.GetEnumerator()) {
     robocopy $pair.Key $pair.Value /E /XO
 }
-``
